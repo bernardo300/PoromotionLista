02ff94b0ac2e3e9f8081681ef06f8b27c9fe964d
bernardo300 <bernardolopes.luz@hotmail.com>
2021-08-31 22:30:28 -0300

    Estilo formulario

--- web/src/components/promotion/form/PromotionForm.js
+++ web/src/components/promotion/form/PromotionForm.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
+import './PromotionForm.css'
 const PromotionForm = (props) => {
   const {id} = useParams()
 return (
   <div>
-    <h2>Formulario id: {id}</h2>
+    <h2>Nova Promoção</h2>
+    <form className="promotion-form__group">
+      <label htmlFor="title">Titulo</label>
+      <input type="text" name="title" id="title"/>
+
+      <label htmlFor="url">Url</label>
+      <input type="text" name="url" id="url"/>
+
+      <label htmlFor="imgUrl">Imagem (URL)</label>
+      <input type="text" name="imgUrl" id="imgUrl"/>
+
+      <label htmlFor="number">Preço</label>
+      <input type="number" name="imgUrl" id="number"/>
+      <div>
+        <button type="submit">Salvar</button>
+      </div>
+      
+
+    </form>
   </div>
 )
 }
