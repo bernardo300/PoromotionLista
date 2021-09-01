59fb98ba960843df9fde557b93a8f20739c0c341
bernardo300 <bernardolopes.luz@hotmail.com>
2021-09-01 11:14:13 -0300

    pegar valor do input

--- web/src/components/promotion/form/PromotionForm.js
+++ web/src/components/promotion/form/PromotionForm.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useParams} from 'react-router-dom'
 import './PromotionForm.css'
+
+const initValue = {
+  'title':'',
+  'url':'',
+  'imageUrl':'',
+  'price':''
+}
 const PromotionForm = (props) => {
+
+  const [values, setValues] = useState({})
   const {id} = useParams()
+
+  function onChange(ev){
+    const {name, value} = ev.target;
+    
+    setValues({...values,[name]:value})
+    console.log(values)
+  }
 return (
   <div>
     <h2>Nova Promoção</h2>
     <form className="promotion-form__group">
       <label htmlFor="title">Titulo</label>
-      <input type="text" name="title" id="title"/>
+      <input type="text" name="title" id="title" onChange={onChange}/>
 
       <label htmlFor="url">Url</label>
-      <input type="text" name="url" id="url"/>
+      <input type="text" name="url" id="url" onChange={onChange}/>
 
-      <label htmlFor="imgUrl">Imagem (URL)</label>
-      <input type="text" name="imgUrl" id="imgUrl"/>
+      <label htmlFor="imageUrl">Imagem (URL)</label>
+      <input type="text" name="imageUrl" id="imageUrl" onChange={onChange}/>
 
-      <label htmlFor="number">Preço</label>
-      <input type="number" name="imgUrl" id="number"/>
+      <label htmlFor="price">Preço</label>
+      <input type="number" name="price" id="price" onChange={onChange}/>
       <div>
         <button type="submit">Salvar</button>
       </div>
